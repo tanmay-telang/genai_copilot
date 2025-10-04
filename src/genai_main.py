edcc48dcd9de468f17cc7dd4fcb47d1f1610e535
Tanmay Telang <tanmay.telang@Tanmays-MacBook-Air.local>
2025-10-05 01:34:34 +0530

    feat: add genai api script to test model
    - add code to run run gemma model using ollama

--- src/genai_main.py
+++ src/genai_main.py
@@ -1,4 +1,31 @@
-"""This code would be used to test genai APIs"""
+"""This code would be used to test pilot run of gen ai model"""
+import ollama
 
 
-print("hello world")
+def get_llm_response(query):
+    """This method is used to query an llm and get response from it"""
+
+    response = ollama.chat(model="gemma2:2b",
+                           messages=[{'role':'user','content': query}])
+    
+    return response.get("message").get('content')
+
+
+def run_llm():
+    """This method is used to run llm"""
+
+    while True:
+        user_input = input("Please enter your query:")
+        if user_input == "quit":
+            break
+        output = get_llm_response(user_input)
+        print(output)
+
+
+run_llm()
+
+
+
+
+
+
