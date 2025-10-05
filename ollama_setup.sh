670ea7d3720ee0c52f157b45caadd5839f0cbc50
Tanmay Telang <tanmay.telang@Tanmays-MacBook-Air.local>
2025-10-05 11:11:36 +0530

    feat(ollama-server-start): add code to start a ollama server
    -add code to start ollama server

--- ollama_setup.sh
+++ ollama_setup.sh
@@ -1,4 +1,7 @@
 # This script should only be ran if ollama is not installed on your system
 
 curl -fsSL https://ollama.com/install.sh | sh
+nohup ollama serve > ollama.log 2>&1 &
 ollama --version
+ollama pull gemma:2b
+ollama list
